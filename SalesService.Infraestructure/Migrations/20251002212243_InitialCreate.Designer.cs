68b8b62974a48be9e8668a7b709a0171300eebce
AguirreDiegExe <diegoexequielaguirre@gmail.com>
2025-10-05 21:01:00 -0300

    Commit antes de merge de la rama de milton

--- SalesService.Infraestructure/Migrations/20251002212243_InitialCreate.Designer.cs
+++ SalesService.Infraestructure/Migrations/20251002212243_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesService.Infraestructure;
 
@@ -10,9 +11,11 @@ using SalesService.Infraestructure;
 namespace SalesService.Infraestructure.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002212243_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
